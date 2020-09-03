85cce48977d5ee6a15bf6512d82094263fe29657
Sam-Gera <samuel.j.gera@gmail.com>
2020-09-02 23:45:34 -0600

    Add project files.

---
+++ Console Application/TestEvaluator.cs
@@ -0,0 +1,105 @@
+﻿using FormulaEvaluator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_Application
+{
+    class TestEvaluator
+    {
+        private static void TestRemoveWhiteSpace()
+        {
+            Console.WriteLine("\nTesting RemoveWhiteSpace");
+            string[] testStrings = new string[3];
+            testStrings[0] = "50 ";
+            testStrings[1] = " 10";
+            testStrings[2] = "   20   ";
+            testStrings = Evaluator.RemoveWhitespace(testStrings);
+            Console.WriteLine(testStrings[0] + "," + testStrings[1] + "," + testStrings[2]);
+        }
+
+        private static void TestIsOperator()
+        {
+            Console.WriteLine("\nTesting IsOperator method");
+            string[] testOperators = new string[7];
+            testOperators[0] = "+";
+            testOperators[1] = "-";
+            testOperators[2] = "/";
+            testOperators[3] = "*";
+            testOperators[4] = "(";
+            testOperators[5] = ")";
+            testOperators[6] = "10";
+            for(int i = 0;i < testOperators.Length;i++)
+            {
+                Console.WriteLine(testOperators[i] + ": " + Evaluator.IsOperator(testOperators[i]));
+            }
+        }
+
+        private static void TestIsInteger()
+        {
+            Console.WriteLine("\nTesting IsInteger method");
+            Random rand = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                int randomInt = rand.Next(0, 1000);
+                Console.WriteLine(randomInt + ": " + Evaluator.IsInteger(randomInt.ToString()));
+            }
+            string[] notInts = new string[5];
+            notInts[0] = "A";
+            notInts[1] = "+";
+            notInts[2] = "B10";
+            notInts[3] = "BC";
+            notInts[4] = " ";
+            for(int i = 0; i < notInts.Length; i++)
+            {
+                Console.WriteLine(notInts[i] + ": " + Evaluator.IsInteger(notInts[i]));
+            }
+        }
+
+        private static void TestIsVariable()
+        {
+            Console.WriteLine("\nTesting IsVariable method");
+            string[] testVars = new string[7];
+            testVars[0] = "A10";
+            testVars[1] = "B6";
+            testVars[2] = "C100";
+            testVars[3] = "D500";
+            testVars[4] = "E50";
+            testVars[5] = "10";
+            testVars[6] = "AaBb10";
+            for(int i = 0; i < testVars.Length; i++)
+            {
+                Console.WriteLine(testVars[i] + ": " + Evaluator.IsVariable(testVars[i]));
+            }
+        }
+
+        private static void TestEvaluate()
+        {
+            Console.WriteLine("\nTesting Evaluate Method");
+            Evaluator.Lookup findVar = new Evaluator.Lookup(search);
+            Console.WriteLine("5 + 10 + 50 * (10/5) + 5 = " + Evaluator.Evaluate("5 + 10 + 50 * (10/5) + 5", findVar));
+            Console.WriteLine("10 / 5 + 20 * (50 * 10 + 5) + 5 = " +  Evaluator.Evaluate("10 / 5 + 20 * (50 * 10 + 5) + 5", findVar));
+            Console.WriteLine("50 / A10 = " + Evaluator.Evaluate("50 / A10", findVar));
+            Console.WriteLine("50 + 10 - 5 * 10 = " +  Evaluator.Evaluate("50 + 10 - 5 * 10", findVar));
+            Console.WriteLine("10 * 50 + 5 - 7 * (10 - 10 + (5 + 5)) * 10 = " + Evaluator.Evaluate("10 * 50 + 5 - 7 * (10 - 10 + (5 + 5)) * 10", findVar));
+
+        }
+
+        private static int search(string s)
+        {
+            if (s == "A10")
+                return 10;
+            return 5;
+        }
+        
+        static void Main(string[] args)
+        {
+            TestRemoveWhiteSpace();
+            TestIsOperator();
+            TestIsInteger();
+            TestIsVariable();
+            TestEvaluate();
+        }
+    }
+}
---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            //Parse Tokens
+            string[] tokens = RemoveWhitespace(Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)"));
+
+            Stack<String> operators = new Stack<String>(); //stack for operator tokens: +, -, /, *, (, )
+            Stack<int> values = new Stack<int>(); //stack for integer values
+            values.Push(int.Parse(tokens[0]));
+            //Iterate over array of parsed tokens
+            for(int i = 1; i < tokens.Length; i++)
+            {
+               
+                string token = tokens[i]; //current token
+                string topOfOperators = " "; //ensures we don't peek the top of the stack when it is empty to avoid getting exceptions we don't want
+                if (operators.Count > 0)
+                    topOfOperators = operators.Peek();
+                if(IsInteger((token)))
+                {
+                    int intToken = int.Parse(token); // recieve int from string token
+                    if (topOfOperators == "/" || topOfOperators == "*")
+                    {
+                        //Helper method used when operators has a / or * at top of stack and we want to backtrack to calculate product
+                        BacktrackProduct(operators, values, intToken);
+                    }
+                    else
+                        values.Push(intToken);
+                }
+                else if(IsOperator(token))
+                {
+                    //Use switch to specify action for each unique operator (instead of multiple if)
+                    switch (token)
+                    {
+                        case "+":case "-":
+                            if (topOfOperators == "+" || topOfOperators == "-")
+                            {
+                                //Helper method used when operators have a + or - at top of stack and we want to calculate sum 
+                                BacktrackSum(operators, values);
+                            }
+                            operators.Push(token);
+                            break;
+
+                        case "/": case "*":
+                            operators.Push(token);
+                            break;
+             
+                        case "(":
+                            operators.Push(token);
+                            break;
+
+                        case ")":
+                            if(topOfOperators == "+" || topOfOperators == "-")
+                            {
+                                BacktrackSum(operators, values);
+                            }
+                            if(operators.Pop() != "(") 
+                            {
+                                //handle possible invalid expression if no opening paranthese was passed
+                                throw new ArgumentException("Invalid expression passed. " +
+                                    "Could not find opening paranethese");
+                            }
+                            if(operators.Count > 0) { 
+                                if (operators.Peek() == "*" || operators.Peek() == "/")
+                                {
+                                    //return ArgumentException when an invalid expression was passed given the minimum amount of integers are not in values
+                                    if (values.Count < 2)
+                                    {
+                                        throw new ArgumentException("Invalid expression passed. Unable to" +
+                                            "divide or multiply with fewer than two remaining values");
+                                    }
+                                    if (operators.Peek() == "*")
+                                        values.Push(values.Pop() * values.Pop());
+                                    else if (operators.Peek() == "/")
+                                    {
+                                        //use temp variables to allow us to divide in the correct order and push the result onto values
+                                        int divisor = values.Pop();
+                                        int dividend = values.Pop();
+                                        //prevent division by zero
+                                        if (divisor == 0)
+                                            throw new ArgumentException("Invalid expression. Cannot divide by 0");
+                                        values.Push(dividend / divisor);
+                                    }
+                                    operators.Pop();
+                                }
+                            }
+                            break;
+                    }
+                }
+                else if (IsVariable(token))
+                {
+                    //initialize theoretical integer variable before try/catch
+                    int varValue = 0;
+                    try
+                    {
+                        //use the variableEvaluator function to grab its integer values
+                        varValue = variableEvaluator(token);
+                    }
+                    //catch possible exception if variableEvaluator does not have an integer value for specified variable
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Invalid variable name " + token + " was found within the expression");
+                    }
+                    if (topOfOperators == "/" || topOfOperators == "*")
+                    {
+                        //Helper method
+                        BacktrackProduct(operators, values, varValue);
+                  
+                    }
+                    else
+                        values.Push(varValue);
+
+                }
+            }
+            //end of iteration over all tokens 
+
+            //if operators is empty return last remaining integer in values stack
+            if (operators.Count == 0)
+            {
+                if (values.Count != 1)
+                    throw new ArgumentException("Invalid expression. Execution ended without one remaining integer in the value stack");
+                return values.Pop(); //return last values in values assuming ArgumentException is not thrown
+            }
+            //checking for invalid conditions upon completion of iteration over tokens
+            if ((operators.Peek() != "+" && operators.Peek() != "-") || values.Count != 2)
+                throw new ArgumentException("Invalid expression. Execution ended with invalid operators and/or remaining integers");
+            //condition if last operator is +
+            if (operators.Peek() == "+")
+            {
+                operators.Pop();
+                return values.Pop() + values.Pop();
+            }
+            //assumiung the only remaining case is operators remains with "-" and two integers remain in values stack
+            int second = values.Pop();
+            int first = values.Pop();
+            operators.Pop(); 
+            return first - second;
+
+
+        }
+
+        /// <summary>
+        ///checks if token is an operator 
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>whether the current token is an operator</returns>
+        public static bool IsOperator(String token)
+        {
+            if (token == "+" || token == "-" ||  token == "(" || token == ")" || token == "/" || token == "*")
+                return true;
+            return false; 
+        }
+
+        /// <summary>
+        /// checks if current token is an integer value
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>whether token is an integer</returns>
+        public static bool IsInteger(String token)
+        {
+            return int.TryParse(token, out _);
+        }
+
+        /// <summary>
+        /// checks if current token is a variable to be evaluated by variableEvaluator
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>whether token is a variable</returns>
+        public static bool IsVariable(string token)
+        {
+            if (Regex.IsMatch(token, @"([a-zA-z]+)(\d+)")) 
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Helper method to remove all trailing and leading whitespace on tokens 
+        /// in Evaluate method 
+        /// </summary>
+        /// 
+        /// <param name="tokens"> array of parsed tokens from expression string</param>
+        /// <returns></returns>
+        public static string[] RemoveWhitespace(string[] tokens)
+        {
+            for(int i = 0; i < tokens.Length; i++)
+            {
+                tokens[i] = tokens[i].Trim();
+            }
+            return tokens;
+        }
+
+        /// <summary>
+        /// Private helper method made to improve readability of code. 
+        /// This method backtracks and calculates the sum/difference of the values in values stack using the most
+        /// recent operator in operators when appropriate
+        /// </summary>
+        /// 
+        /// <param name="operators"> operator stack containing mathematical operators</param>
+        /// <param name="values"> values stack containing current integers in calculation </param>
+        /// 
+        private static void BacktrackSum(Stack<string> operators, Stack<int> values)
+        {
+            //Check for possible invalid expression 
+            if (values.Count < 2)
+            {
+                throw new ArgumentException("Invalid Argument. Unable to add or subtract with " +
+                "fewer than two remaining values");
+            }
+            if (operators.Peek() == "+")
+            {
+                values.Push(values.Pop() + values.Pop());
+            }
+            else
+            {
+                //use temporary variables to subtract in the correct order (subtracting the integer from on top of the stack from the integer below would be the correct operation in reverse)
+                int second = values.Pop();
+                int first = values.Pop();
+                values.Push(first - second);
+            }
+            //Remove operator from top of operator stack once we are done using it
+            operators.Pop();
+        }
+
+        /// <summary>
+        /// Private helper method made to improve readability of code. 
+        /// This method backtracks and calculates the product/quotient of the values in values stack using the most
+        /// recent operator in operators when appropriate
+        /// </summary>
+        /// 
+        /// <param name="operators"> operator stack containing mathematical operators</param>
+        /// <param name="values"> values stack containing current integers in calculation </param>
+        /// <param name="token"> current Integer token</param>
+        private static void BacktrackProduct(Stack<string> operators, Stack<int> values, int token)
+        {
+            //check for a possible invalid expression
+            if (values.Count == 0)
+            {
+                throw new ArgumentException("Invalid expression. Unable to multiply or divide with fewer than " +
+                    "two remaining values");
+            }
+            if (operators.Peek() == "*")
+                values.Push(values.Pop() * token);
+            else
+            {
+                //Prevent division by 0
+                if (token == 0)
+                    throw new ArgumentException("Invalid expression. Cannot divide by 0");
+                values.Push(values.Pop() / token);
+            }
+            //Remove operator from top of operator stack once we are done using it 
+            operators.Pop();
+        }
+    }
+}
